8e29ee9c7d7c4f02c34d8afcd72a848db298e8b5
Corey Fuller <coreyfuller00@gmail.com>
2020-11-24 14:42:46 -0500

    Adding script to download files and run code

---
+++ file-download.ps1
@@ -0,0 +1,19 @@
+$invocation = (Get-Variable MyInvocation).Value
+$directory =  Split-Path $invocation.MyCommand.Path
+$urls = 'http://www.textfiles.com/etext/FICTION/2city10.txt',
+        'http://www.textfiles.com/etext/FICTION/anna_karenina',
+        'http://www.textfiles.com/etext/FICTION/2000010.txt',
+        'http://www.textfiles.com/etext/FICTION/center_earth',
+        'http://www.textfiles.com/etext/FICTION/moon10a.txt'
+
+
+foreach ($url in $urls) {
+    $paths = $url.Split("/")
+    $file = $paths[$paths.Length - 1]
+    if (-not $file.Contains(".txt")){
+        $file = $file + ".txt"  
+    }
+    wget $url -OutFile $directory/$file
+}
+
+python .\encrypt.py
